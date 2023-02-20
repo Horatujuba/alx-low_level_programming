baa85f1ef8109dc3e47a9d881651865a9c5129a5
Horatujuba <fitsumtujuba22@gmail.com>
2023-02-20 14:15:54 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_v10 - prints alphabet 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{char c;
+	int i=0;
+	while (i<10)
+	{
+		c='a';
+		while(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
+
