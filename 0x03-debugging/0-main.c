11d34f34fb6c67ba09aab841203984e7ac949a82
Horatujuba <fitsumtujuba22@gmail.com>
2023-02-23 11:37:52 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Tests function thet prints if intger is positive or negative 
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
