ce3730b6e26be21f51174b7197ac482c9018b823
Horatujuba <fitsumtujuba22@gmail.com>
2023-02-28 11:09:08 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,7 @@
 void puts_half(char *str)
 
 {
-	int j = 0;
+	/*  j = 0;
 	int k;
 
 	while (str[j] != '\n')
@@ -30,6 +30,20 @@ void puts_half(char *str)
 	for (; k < j; k++)
 	{
 	_putchar(str[k]);
-	}
+	}*/
+	int a, n, i;
+
+	i = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		i++;
+
+	n = (i / 2);
+
+	if ((i % 2) == 1)
+		n = ((i + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
 	_putchar('\n');
 }
