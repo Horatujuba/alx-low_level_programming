4c4716370c1e698df997cd6dead23ef9556f6c5a
Horatujuba <fitsumtujuba22@gmail.com>
2023-02-28 10:52:46 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,26 +8,19 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-/*	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-	dest[i] = src[i];
-	}
-	dest[i++] = '\0';
-	*/
 
 	int l = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + l) != '\0')
 	{
 		l++;
 	}
 	for ( ; x < l ; x++)
 	{
-		dest[0] = src[x];
+		dest[x] = src[x];
 	}
-	dect[l] = '\0';
-	Return (dest);
+	dest[l] = '\0';
+
+	return (dest);
 }
