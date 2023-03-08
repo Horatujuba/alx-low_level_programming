ad937276f69a7516657a648fe2e6808444041b04
Horatujuba <fitsumtujuba22@gmail.com>
2023-03-08 12:01:23 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calc the lent of str
+ * @s: z str to count
+ *
+ * Return: int val
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\n')
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
