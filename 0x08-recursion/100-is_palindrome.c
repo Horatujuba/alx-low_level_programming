5b67e483d82def3104df86c0e634e03ebc61dc1e
Horatujuba <fitsumtujuba22@gmail.com>
2023-03-09 10:13:20 -0800

    100-palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns z l of strl
+ * @s: str
+ * Return: z l pf str
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else 
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * comparator - cmp eeach char
+ * @s: string
+ * @n1: smallest it
+ * @n2: biggest it
+ * Return: .
+ * */
+
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - detects str
+ * @s: str
+ * Return: 1 if s is p, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
+	
+}
