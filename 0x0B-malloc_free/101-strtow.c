7f8626f5e68c6d8e7178e649090e936721a857b0
Horatujuba <fitsumtujuba22@gmail.com>
2023-03-15 13:58:51 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,7 +9,7 @@
 int count_word(char *s)
 {
 	int flag, c, w;
-	
+
 	flag = 0;
 	w = 0;
 	for (c = 0; s[c] != '\0'; c++)
@@ -22,7 +22,7 @@ int count_word(char *s)
 			w++;
 		}
 	}
-	return (w);	
+	return (w);
 }
 /**
  * **strtow - splits a string into words
@@ -31,7 +31,7 @@ int count_word(char *s)
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
-char **strtow(char *str)		
+char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
@@ -47,19 +47,19 @@ char **strtow(char *str)
 	for (i = 0; i <= len; i++)
 	{
 	if (str[i] == ' ' || str[i] == '\0')
+	{
+		if (c)
 		{
-			if (c)
-			{
-				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL)
-				return (NULL);
-				while (start < end)
-					*tmp++ = str[start++];
-				*tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
-				c = 0;
+		end = i;
+		tmp = (char *) malloc(sizeof(char) * (c + 1));
+		if (tmp == NULL)
+		return (NULL);
+		while (start < end)
+		*tmp++ = str[start++];
+		*tmp = '\0';
+		matrix[k] = tmp - c;
+		k++;
+		c = 0;
 		}
 		}
 		else if (c++ == 0)
